0f115159f74e789800d0a6d000f7dffb4502aea3
Francen <sellote.francen519@gmail.com>
2024-05-15 09:20:53 +0800

    Material UI

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Appbar from './Appbar';
+import MaterialUI from './MaterialUI';
+
+
+
+function App() {
+return (
+    <div className="App">
+        <MaterialUI/>
+        <Appbar/>
+    </div>
+);
+}
+
+
+export default App;
+
---
+++ src/MaterialUI.js
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+function MaterialUI() {
+  return <Button variant="contained">Hello, I am Francen P. Sellote</Button>;
+}
+
+export default MaterialUI;
